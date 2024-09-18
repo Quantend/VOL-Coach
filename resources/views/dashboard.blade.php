bf02bb399802f67670a7bb2a786f49392e97620b
bramwilbers <Bram.Wilbers@student.gildeopleidingen.nl>
2024-09-18 14:25:05 +0200

    update homepage

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,16 @@
 <x-app-layout>
+<div class="max-w-7xl mx-auto sm:px-6 lg:px-10 space-y-6 mt-8">
+    <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <div class="max-w-xl">
 
+            <h1 class="text-3xl mb-2">Home</h1>
+            <div> Lorem ipsum dolor sit amet. Et dolorem repudiandae sed possimus voluptas non obcaecati voluptatem qui accusantium galisum. 
+                 Ut maiores tempora non eaque fugit sit cupiditate consequatur.Est explicabo ducimus ut ipsum laudantium aut fugiat eveniet ut optio pariatur ut quae nihil qui ipsum nulla qui quos voluptas. 
+                 Et perspiciatis velit sit eaque nisi sed dolore dicta. Non saepe dolor rem unde perferendis et praesentium sint. 
+                 Aut quae ipsa a corrupti saepe a similique vero in dolorum magni. Et labore vero aut ratione consequuntur qui voluptas velit eos itaque dolorum. 
+                 Quo suscipit veritatis qui neque molestiae qui cumque quisquam a consequatur rerum et voluptas expedita et voluptatem voluptas? </div>
+
+        </div>
+    </div>
+</div>
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,23 +6,24 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img class="block h-24   w-auto fill-current" src="{{asset('gilde.png')}}">
+                        <img class="h-24" src="{{asset('gilde.png')}}">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link class="text-lg" :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Home') }}
                     </x-nav-link>
                 </div>
+                
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="hidden sm:flex sm:items-center">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-lg leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150 text-xl">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
@@ -68,7 +69,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Home') }}
             </x-responsive-nav-link>
         </div>
 

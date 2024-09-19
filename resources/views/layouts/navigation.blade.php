a0a93a81dd90eecd8b9699125938342d6c1a62fc
Dylan Broens <dylan.broens@student.gildeopleidingen.nl>
2024-09-19 14:44:40 +0200

    added hoofdthema to nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,13 @@
                         {{ __('Home') }}
                     </x-nav-link>
                 </div>
-                
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('hoofdthema')" :active="request()->routeIs('hoofdthema')">
+                        {{ __("Hoofdthema's") }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->

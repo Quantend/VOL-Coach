a7923cf0463513294a79311577a42b34e2ba06f7
Tim019283 <tim.neto.rodrigues@student.gildeopleidingen.nl>
2024-10-07 11:28:07 +0200

    style update

--- resources/views/livewire/hoofdthema.blade.php
+++ resources/views/livewire/hoofdthema.blade.php
@@ -1,12 +1,12 @@
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-10 space-y-6 mt-8">
-    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+    <div class="p-4 sm:p-8 sm:rounded-lg">
         <div class="">
             @if($showHoofdthemaList)
                 <h1 class="text-3xl">Hoofdthema's</h1>
-                <ul>
+                <ul class="grid grid-cols-3 gap-4 p-0">
                     @foreach($hoofdthemas as $hoofdthema)
-                        <div class="p-4" wire:click="selectHoofdthema({{ $hoofdthema->id }})">
-                            <p class="text-blue-500 underline text-xl hover:cursor-pointer">{{ $hoofdthema->naam }}</p>
+                        <div class="p-4 hover:cursor-pointer hover:scale-105 transition-all shadow bg-white rounded-md" wire:click="selectHoofdthema({{ $hoofdthema->id }})">
+                            <p class="text-pink-400">{{ $hoofdthema->naam }}</p>
                             <p class="text-gray-700">{{ $hoofdthema->beschrijving }}</p>
                         </div>
                     @endforeach
@@ -15,16 +15,16 @@
 
             @if($selectedHoofdthema)
                 <h2 class="text-3xl">{{ $selectedHoofdthema->naam }}</h2>
-                <button class="text-red-500 hover:cursor-pointer" wire:click="backToHoofdthemaList">Terug naar hoofdthema's</button>
-                    <div class="flex justify-center my-2">
-                        @if(!empty($videoId))
+                <div class="text-red-500 hover:cursor-pointer theme-button" wire:click="backToHoofdthemaList">Terug naar hoofdthema's</div>
+                    @if(!empty($videoId))
+                        <div class="flex justify-center my-2 my-10">
                             <iframe height="400" width="600" controls src="https://www.youtube.com/embed/{{ $videoId }}" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        @endif
-                    </div>
+                        </div>
+                    @endif
                     <div>
                         {!! $selectedHoofdthema->content !!}
                     </div>
-                <button class="text-blue-500 hover:cursor-pointer" wire:click="startZelfscoreToets">Zelfscore toets</button>
+                <div class="text-blue-500 hover:cursor-pointer theme-button" wire:click="startZelfscoreToets">Zelfscore toets</div>
             @endif
         </div>
     </div>

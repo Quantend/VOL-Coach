e0293067d3605f133b2cba9f238a8fd7cf422a16
bramwilbers <Bram.Wilbers@student.gildeopleidingen.nl>
2024-11-20 14:30:09 +0100

    added gilde colors

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-2 pt-2 border-b-2 border-pink-600 text-lg font-medium leading-5 text-pink-600 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+            ? 'inline-flex items-center px-2 pt-2 border-b-2 border-pink-400 text-lg font-medium leading-5 text-pink-400 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out hover:text-pink-300'
             : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-lg font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,21 +15,58 @@ export default {
                 maaxRegular: ['maax-regular', ...defaultTheme.fontFamily.sans],
                 maaxBold: ['maax-bold', ...defaultTheme.fontFamily.sans],
                 maaxMedium: ['maax-medium', ...defaultTheme.fontFamily.sans],
+                arial: ['arial', ...defaultTheme.fontFamily.sans],
+                poppins: ['poppins', ...defaultTheme.fontFamily.sans],
             },
 
             colors: {
                 'pink': {
-                    100: '#f5a3c3',  // Light pink
-                    200: '#f07a9b',  // Medium pink
-                    300: '#f00069',  // Hover (keeping as is)
-                    400: '#dd0069',  // Default (keeping as is)
-                    500: '#b9005f',  // Darker pink
-                    600: '#9b0055',  // Deep pink
+                    100: '#F5A3C3',  // Light pink
+                    200: '#F07A9B',  // Medium pink
+                    300: '#F00069',  // Hover (keeping as is)
+                    400: '#DD0069',  // Default (keeping as is)
+                    500: '#B9005F',  // Darker pink
+                    600: '#9B0055',  // Deep pink
                     700: '#730041',  // Darker tone
+                    800: '#FFFFFF',  // White
                 },
+
+                'blue': {
+                    100: '#A3CFF5',  // Light blue
+                    200: '#7AB4F0',  // Medium blue
+                    300: '#00365E',  // Hover (keeping as is)
+                    400: '#00365E',  // Default (keeping as is)
+                    500: '#0048B9',  // Darker blue
+                    600: '#003D9B',  // Deep blue
+                    700: '#002F73',  // Darker tone
+                    800: '#FFFFFF',  // White
+                },
+
+                'yellow': {
+                    100: '#FFF5A3',  // Light yellow
+                    200: '#FFE87A',  // Medium yellow
+                    300: '#F8E800',  // Hover (keeping as is)
+                    400: '#F8E800',  // Default (keeping as is)
+                    500: '#B9A800',  // Darker yellow
+                    600: '#9B9100',  // Deep yellow
+                    700: '#736B00',  // Darker tone
+                    800: '#FFFFFF',  // White
+                },
+
+                'green': {
+                    100: '#A3F5D2',  // Light green
+                    200: '#7AF0BE',  // Medium green
+                    300: '#00B588',  // Hover (keeping as is)
+                    400: '#00B588',  // Default (keeping as is)
+                    500: '#007F5F',  // Darker green
+                    600: '#006B50',  // Deep green
+                    700: '#004B3A',  // Darker tone
+                    800: '#FFFFFF',  // White
+                },
+               
+            },
             },
         },
-    },
 
     darkMode: false,
 

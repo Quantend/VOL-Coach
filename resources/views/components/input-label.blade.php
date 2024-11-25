f7fc2ab38fc284f32e01def115facf7cbdcf96bc
bramwilbers <Bram.Wilbers@student.gildeopleidingen.nl>
2024-11-25 14:06:07 +0100

    update style

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-md text-gray-700 font-maaxRegular']) }}>
+<label {{ $attributes->merge(['class' => 'block font-medium text-md text-gray-700 font-maaxRegular text-center']) }}>
     {{ $value ?? $slot }}
 </label>

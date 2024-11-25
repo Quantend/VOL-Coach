d0baa418137aa6ec4deb7015e6562e58eb11d6a6
Dylan Broens <dylan.broens@student.gildeopleidingen.nl>
2024-11-25 13:13:14 +0100

    sure

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,8 +18,8 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="no-underline">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('inzichten')" :active="request()->routeIs('inzichten')" class="no-underline">
+                        {{ __('Inzichten') }}
                     </x-nav-link>
                 </div>
 
@@ -99,8 +99,8 @@
 
         <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
             <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="no-underline">
-                    {{ __('Dashboard') }}
+                <x-responsive-nav-link :href="route('inzichten')" :active="request()->routeIs('inzichten')" class="no-underline">
+                    {{ __('Inzichten') }}
                 </x-responsive-nav-link>
             </div>
 

f4094b5a9ea810015c6d1ec561260a61e5b9a259
bramwilbers <Bram.Wilbers@student.gildeopleidingen.nl>
2024-12-09 10:59:05 +0100

    mail

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,4 +1,7 @@
 <h1>Welkom, {{$user->name}}</h1>
 <p>Bedankt voor het regristeren bij de VOL-coach site.</p>
 <p>Mocht je de site nog eens willen bezoeken, staat hieronder de link</p>
-<a href="https://vol-coach.gildedevops.it/">bezoek de website</a>
+<a href="https://vol-coach.gildedevops.it/">bezoek de website</a><br><br>
+<p>Heb je nog een opdracht voor een webiste/applicatue en wil je die door ons laten maken.</p>
+<p>Kun je een mail sturen naar k.vdhulst@rocgilde.nl</p>
+<p>Of kun je bellen naar 06 14962215</p>
